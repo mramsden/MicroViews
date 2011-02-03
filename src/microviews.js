1c027180010c17fb3d57e038871d5eff092e2e25
Marcus Ramsden <cuscus1986@gmail.com>
2011-02-03 23:49:49 +0000

    Created the microviews library and the very initial bootstrap.

---
+++ src/microviews.js
@@ -0,0 +1,49 @@
+var MicroViews = function($) {
+  console.log("MICROVIEWS");
+
+  var JQUERY_SRC = "http://code.jquery.com/jquery-1.5.min.js";	
+
+  var
+    // This will become the MicroViews object.
+    _mv = {},
+    // Storage for user options.
+    opts;
+
+  function isJQueryAvailable() {
+    return !(typeof($) === 'undefined');
+  }
+
+  /**
+   * Initialises the MicroViews library.
+   */
+  _mv.init = function(userOpts) {
+    console.log("Initialising MicroViews");
+    opts = userOpts;
+    if (!isJQueryAvailable()) {
+      console.log("Will load jQuery");
+      // We will need to load jQuery before continuing.
+      var script = document.createElement("script");
+      script.setAttribute("type", "text/javascript");
+      script.setAttribute("src", JQUERY_SRC);
+      if (script.addEventListener) {
+        // DOM compliant
+        script.addEventListener("load", _mv.parse, false);
+      } else {
+        // A pissy comment about IE 8 and earlier
+        script.attachEvent("onload", _mv.parse);
+      }
+      var head = document.getElementsByTagName("head")[0];
+      head.appendChild(script);
+    } else {
+      // Great we have jQuery, so lets get on with parsing straight
+      // away.
+      _mv.parse();
+    }
+  }
+
+  _mv.parse = function() {
+    console.log("MicroViews ready");
+  }
+
+  return _mv;
+};
