872bb9e0a42a015c113cf48eca6601baf2d7631e
Marcus Ramsden <cuscus1986@gmail.com>
2011-02-04 08:30:19 +0000

    Updated the server to import css to the build directory as well. Also updated dependency loading in microviews.

--- src/microviews.js
+++ src/microviews.js
@@ -1,6 +1,7 @@
 var MicroViews = function($) {
   console.log("MICROVIEWS");
 
+  var MICROVIEWS_BASE_URL = "http://localhost:4567";
   var JQUERY_SRC = "http://code.jquery.com/jquery-1.5.min.js";	
 
   var
@@ -13,6 +14,44 @@ var MicroViews = function($) {
     return !(typeof($) === 'undefined');
   }
 
+  function fetchMicroViewsDependencies() {
+    loadJavascript(MICROVIEWS_BASE_URL+"/js/jquery.qtip.pack.js");
+    loadCSS(MICROVIEWS_BASE_URL+"/css/jquery.qtip.min.css");
+  }
+
+  function loadJavascript(src, loadHandler, errorHandler) {
+    var script = document.createElement("script");
+    script.setAttribute("type", "text/javascript");
+    script.setAttribute("src", src);
+    if (script.addEventListener) {
+      script.addEventListener("load", loadHandler, false);
+      script.addEventListener("error", errorHandler, false);
+    } else {
+      script.attachEvent("onload", loadHandler);
+      script.attachEvent("onerror", errorHandler);
+    }
+    
+    var head = document.getElementsByTagName("head")[0];
+    head.appendChild(script);
+  }
+  
+  function loadCSS(src, loadHandler, errorHandler) {
+    var link = document.createElement("link");
+    link.setAttribute("type", "text/css");
+    link.setAttribute("rel", "stylesheet");
+    link.setAttribute("href", src);
+    if (link.addEventListener) {
+      link.addEventListener("load", loadHandler, false);
+      link.addEventListener("error", errorHandler, false);
+    } else {
+      link.attachEvent("onload", loadHandler);
+      link.attachEvent("onerror", errorHandler);
+    }
+    
+    var head = document.getElementsByTagName("head")[0];
+    head.appendChild(link);
+  }
+
   /**
    * Initialises the MicroViews library.
    */
@@ -20,22 +59,9 @@ var MicroViews = function($) {
     opts = userOpts;
     if (!isJQueryAvailable()) {
       // We will need to load jQuery before continuing.
-      var script = document.createElement("script");
-      script.setAttribute("type", "text/javascript");
-      script.setAttribute("src", JQUERY_SRC);
-      if (script.addEventListener) {
-        // DOM compliant
-        script.addEventListener("load", _mv.parse, false);
-      } else {
-        // A pissy comment about IE 8 and earlier
-        script.attachEvent("onload", _mv.parse);
-      }
-      var head = document.getElementsByTagName("head")[0];
-      head.appendChild(script);
+      loadJavascript(JQUERY_SRC, fetchMicroViewsDependencies());
     } else {
-      // Great we have jQuery, so lets get on with parsing straight
-      // away.
-      _mv.parse();
+      fetchMicroViewsDependencies();
     }
   }
 
